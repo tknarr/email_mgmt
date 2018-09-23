13f85c1b4eb29d41692fa965015ec8e0b7fb75d7
Todd Knarr <tknarr@silverglass.org>
2018-09-22 21:17:50 -0700

    Fix maildir permissions Makei maildir base path a variable

--- scripts/vmaildir.sh
+++ scripts/vmaildir.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
 op="$1"
+maildir_base=/home/vmail/u
 
 case ${op} in
 
     create)
         u=`echo "$2" | tr -dc '[:alnum:]_.'`
-        if [ -e /home/vmail/${u} ]
+        if [ -e ${maildir_base}/${u} ]
         then
-            echo "Target already exists: /home/vmail/${u}"
+            echo "Target already exists: ${maildir_base}/${u}"
             exit 1
         fi
-        mkdir /home/vmail/${u} && chmod u=rwx,go= /home/vmail/${u}
+        mkdir ${maildir_base}/${u} && chmod ug=rwx,o= ${maildir_base}/${u}
         ;;
 
     rename)
         u=`echo "$2" | tr -dc '[:alnum:]_.'`
         n=`echo "$3" | tr -dc '[:alnum:]_.'`
-        if [ ! -e /home/vmail/${u} ]
+        if [ ! -e ${maildir_base}/${u} ]
         then
-            echo "Source does not exist: /home/vmail/${u}"
+            echo "Source does not exist: ${maildir_base}/${u}"
             exit 1
         fi
-        if [ -e /home/vmail/${n} ]
+        if [ -e ${maildir_base}/${n} ]
         then
-            echo "Target already exists: /home/vmail/${n}"
+            echo "Target already exists: ${maildir_base}/${n}"
             exit 1
         fi
-        mv /home/vmail/${u} /home/vmail/${n}
+        mv ${maildir_base}/${u} ${maildir_base}/${n}
         ;;
 
     delete)
         u=`echo "$2" | tr -dc '[:alnum:]_.'`
-        if [ ! -e /home/vmail/${u} ]
+        if [ ! -e ${maildir_base}/${u} ]
         then
-            echo "Target does not exist: /home/vmail/${u}"
+            echo "Target does not exist: ${maildir_base}/${u}"
             exit 1
         fi
-        rm -rf /home/vmail/${u}
+        rm -rf ${maildir_base}/${u}
+        ;;
+
+    *)
+        echo "Unrecognized command: ${op}"
+        exit 127
         ;;
 esac
