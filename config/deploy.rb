753892499c3044584c78ecece15cae8edba849a6
Todd Knarr <tknarr@silverglass.org>
2018-09-24 20:40:40 -0700

    Prepare Capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/home/email_mgmt/app"
 append :linked_files, "config/master.key", "config/credentials.yml.enc", "config/database.yml", "config/settings/production.local.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "config/settings"
+append :linked_dirs, ".bundle", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "config/settings"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +33,12 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# Rails configuration items
+set :conditionally_migrate, true
+set :migration_role, :app
+set :keep_assets, 3
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -14,7 +14,6 @@ server 'dev1.silverglass-tech.com', roles: %{web app db}
 # Global options
 # --------------
 set :ssh_options,
-    keys_only: true,
     keys: %w[/home/tknarr/.ssh/id_awsdev],
-    forward_agent: false,
     auth_methods: %w[publickey],
+    forward_agent: true
