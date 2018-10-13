67fbb86fd356840f3725a922a0e7093372e51a13
Todd Knarr <tknarr@silverglass.org>
2018-10-12 21:47:53 -0700

    Restore ignored files that will be overridden by shared files

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,17 @@
 
 # Alias accounts not currently used/supported properly.
 
+# Account types, mandatory, leave these as-is
 AcctType.create!(code: 'R', description: 'Root', abbreviation: 'Root', home_root: '/')
 AcctType.create!(code: 'S', description: 'System user', abbreviation: 'Sys', home_root: '/home/')
-# AcctType.create!(code: 'A', description: 'Alias', abbreviation: 'Alias', home_root: '')
 AcctType.create!(code: 'V', description: 'Virtual user', abbreviation: '', home_root: '/home/vmail/u/',
                  uid: 'vmail', gid: 'vmail', transport: 'lmtp:unix:private/dovecot-lmtp')
+# AcctType.create!(code: 'A', description: 'Alias', abbreviation: 'Alias', home_root: '')
 
+# Root account, mandatory
 MailUser.create!(username: 'root', password: 'changeme', acct_type: 'R', admin: 1)
+
+# Standard alias users, not currently supported
 # MailUser.create(username: 'postmaster', password_digest: 'x', acct_type: 'A')
 # MailUser.create(username: 'hostmaster', password_digest: 'x', acct_type: 'A')
 # MailUser.create(username: 'webmaster', password_digest: 'x', acct_type: 'A')
@@ -17,6 +21,7 @@ MailUser.create!(username: 'root', password: 'changeme', acct_type: 'R', admin: 
 # MailUser.create(username: 'noc', password_digest: 'x', acct_type: 'A')
 # MailUser.create(username: 'security', password_digest: 'x', acct_type: 'A')
 
+# Standard routing entries for users that should always exist per ICANN policy
 MailRouting.create!(address_user: 'root', address_domain: '*', recipient: 'root')
 MailRouting.create!(address_user: 'admin', address_domain: '*', recipient: 'root')
 MailRouting.create!(address_user: 'administrator', address_domain: '*', recipient: 'root')
